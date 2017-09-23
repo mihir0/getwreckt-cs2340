4570a02283da051bc15db98ddf344231e5c617d8
mviust3 <mviust3@gatech.edu>
2017-09-23 16:13:13 -0400

    implemented registration activity

--- RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/controller/RegisterActivity.java
+++ RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/controller/RegisterActivity.java
@@ -1,12 +1,20 @@
 package getwreckt.cs2340.rattrack.controller;
 
 import android.os.Bundle;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.util.Log;
+import android.view.View;
+import android.content.Intent;
+
+import java.util.IllegalFormatCodePointException;
 
 import getwreckt.cs2340.rattrack.R;
+import getwreckt.cs2340.rattrack.model.*;
 
 /**
  * Created by maya v on 9/21/2017.
@@ -15,12 +23,50 @@ import getwreckt.cs2340.rattrack.R;
 public class RegisterActivity extends AppCompatActivity {
 
     private AutoCompleteTextView userField;
-    private EditText passField;
+    private TextInputEditText passField;
     private Button registerButn;
+    private TextInputLayout til;
+    private User _user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        /**
+         * Grab dialog widgets
+         */
+
+        userField = (AutoCompleteTextView) findViewById(R.id.username);
+        passField = (TextInputEditText) findViewById(R.id.password);
+        til = (TextInputLayout) findViewById(R.id.text_input_layout);
+
+        onRegPressed();
+    }
+
+    public void onRegPressed() {
+        Log.d("Register", "Register new user");
+        Model model = Model.getInstance();
+
+        registerButn = (Button) findViewById(R.id.register);
+        registerButn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toInAppScreen = new Intent(RegisterActivity.this,
+                        InAppActivity.class);
+                String username = userField.getText().toString();
+                String password = passField.getText().toString();
+
+                try {
+                    UserList.addUser(username, password);
+                    startActivity(toInAppScreen);
+
+                } catch (IllegalArgumentException iae) {
+                    til.setError("Invalid username or password. Try Again.");
+                }
+
+            }
+        });
+
     }
 }
