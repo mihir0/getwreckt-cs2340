7b5adef2cd2f69c0d11247ce57a1a880d51f8a2e
Manan Patel <mpatel360@gatech.edu>
2017-09-23 11:59:45 -0400

    edited LoginActivity onCreate() method

--- RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/controller/LoginActivity.java
+++ RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/controller/LoginActivity.java
@@ -1,6 +1,8 @@
 package getwreckt.cs2340.rattrack.controller;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import getwreckt.cs2340.rattrack.R;
 
 
 
@@ -9,5 +11,8 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class LoginActivity extends AppCompatActivity {
-
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
 }
