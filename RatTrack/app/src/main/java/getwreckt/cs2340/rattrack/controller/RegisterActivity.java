b286937202b1eafc8da1572a88b5d293ac757001
Manan Patel <mpatel360@gatech.edu>
2017-09-23 13:10:03 -0400

    edited WelcomeActivity to initiate login or initiate registrationdepending on button pressed

--- RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/controller/RegisterActivity.java
+++ RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/controller/RegisterActivity.java
@@ -1,11 +1,18 @@
 package getwreckt.cs2340.rattrack.controller;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
+import getwreckt.cs2340.rattrack.R;
 
 /**
  * Created by maya v on 9/21/2017.
  */
 
 public class RegisterActivity extends AppCompatActivity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
 }
