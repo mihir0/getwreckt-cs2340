c10a12486394ddaa216d0282d0306c15901c0e6b
Manan Patel <mpatel360@gatech.edu>
2017-09-28 18:36:29 -0400

    added Admin class

---
+++ RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/model/Admin.java
@@ -0,0 +1,11 @@
+package getwreckt.cs2340.rattrack.model;
+
+/**
+ * Created by Patel on 9/28/2017.
+ */
+
+public class Admin extends User {
+    public Admin(String username, String password) {
+        super(username, password);
+    }
+}
