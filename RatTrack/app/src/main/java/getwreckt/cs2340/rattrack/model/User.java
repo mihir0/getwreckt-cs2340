4e14bc76a1c4bf9b3d269421517c9daab28a07f8
mviust3 <mviust3@gatech.edu>
2017-09-28 18:06:25 -0400

    added first and last name variables to User

--- RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/model/User.java
+++ RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/model/User.java
@@ -5,22 +5,43 @@ package getwreckt.cs2340.rattrack.model;
  */
 
 public class User {
+    private String firstName;
+    private String lastName;
     private String userName;
     private String pass;
+
     public User(String userName, String pass) {
+        this.userName = userName;
+        this.pass = pass;
+    }
+
+    public User(String firstName, String lastName, String userName, String pass) {
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.userName = userName;
         this.pass = pass;
     }
     public String getUserName() {
         return this.userName;
     }
+
     public void setUserName(String userName) {
         this.userName = userName;
     }
+
     public String getPass() {
         return this.pass;
     }
+
     public void setPass(String pass) {
         this.pass = pass;
     }
+
+    public String getFirstName() {return this.firstName;}
+
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+
+    public String getLastName() {return this.lastName;}
+
+    public void setLastName(String lastName) {this.lastName = lastName;}
 }
