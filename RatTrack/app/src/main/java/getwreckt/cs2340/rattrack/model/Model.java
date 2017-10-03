bce9f648872b981e6275cb79d3a4487550a48830
Manan Patel <mpatel360@gatech.edu>
2017-10-02 20:53:03 -0400

    created a Model instance for default user in Login

--- RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/model/Model.java
+++ RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/model/Model.java
@@ -6,18 +6,39 @@ package getwreckt.cs2340.rattrack.model;
 
 public class Model {
     private static final Model _instance = new Model();
+
+    /**
+     * The private Model instance.
+     * @return the private Model instance
+     */
     public static Model getInstance() {return _instance;}
+
+    /**
+     * Creates a Model with dummy data.
+     */
     private Model() {
         loadDummyData();
     }
+
     private User currentUser;
 
+    /**
+     * Adds a sample user to the Model.
+     */
     private void loadDummyData() {
         UserList.addUser(new User("user", "password"));
     }
 
+    /**
+     * Current user in the system.
+     * @return current user
+     */
     public User getCurrentUser() {return this.currentUser;}
 
+    /**
+     * Sets the current user to {@code currentUser}.
+     * @param currentUser the user to set as the current user
+     */
     public void setCurrentUser(User currentUser) {this.currentUser = currentUser;}
 
 }
--- RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/model/User.java
+++ RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/model/User.java
@@ -8,38 +8,74 @@ public class User {
     private String fullName;
     private String userName;
     private String pass;
+
+    /**
+     * Create a User with username {@code userName} and password {@code pass}.
+     * @param userName the username for the user
+     * @param pass the password for the user
+     */
     public User(String userName, String pass) {
         this.fullName = "Default Name";
         this.userName = userName;
         this.pass = pass;
     }
 
+    /**
+     * Creates a user with full name {@code fullName}, username {@userName}, and password {@code pass}.
+     * @param fullName the first and last name of the new user
+     * @param userName the username of the new user
+     * @param pass the password of the new user
+     */
     public User(String fullName, String userName, String pass) {
         this.fullName = fullName;
         this.userName = userName;
         this.pass = pass;
     }
 
+    /**
+     * Username of the user
+     * @return username of the user
+     */
     public String getUserName() {
         return this.userName;
     }
 
+    /**
+     * Set username of User to {@code userName}
+     * @param userName the username to change
+     */
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+    /**
+     * Password of the user
+     * @return username of the user
+     */
     public String getPass() {
         return this.pass;
     }
 
+    /**
+     * Set password of User to {@code pass}
+     * @param pass the password to change
+     */
     public void setPass(String pass) {
         this.pass = pass;
     }
 
+    /**
+     * Full name of the user
+     * @return full name of the user
+     */
     public String getFullName() {
         return this.fullName;
     }
 
+    /**
+     * Set full name of User to {@code fullName}
+     * @param fullName the full name to change
+     */
     public void setFullName(String fullName) {
         this.fullName = fullName;
     }
