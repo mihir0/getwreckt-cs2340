04f55c921fef08562975fd481999c5a23b68724f
mviust3 <mviust3@gatech.edu>
2017-10-05 17:57:05 -0400

    created readCSVFile() method in Model.java

--- RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/model/Model.java
+++ RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/model/Model.java
@@ -1,5 +1,14 @@
 package getwreckt.cs2340.rattrack.model;
 
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import getwreckt.cs2340.rattrack.R;
+
 /**
  * Created by maya v on 9/21/2017.
  */
@@ -19,5 +28,27 @@ public class Model {
     public User getCurrentUser() {return this.currentUser;}
 
     public void setCurrentUser(User currentUser) {this.currentUser = currentUser;}
+
+    public void readCSVFile(InputStream is) {
+
+        try {
+            //InputStream is = getResources().openRawResource(R.raw.Rat_Sightings);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+
+            String line;
+            br.readLine(); //get rid of header line
+            while((line = br.readLine()) != null) {
+                Log.d("Model", line);
+                String[] sightData = line.split(",");
+
+                //add new Sighting to list of sightings
+
+            }
+            br.close();
+
+        } catch (IOException e) {
+            Log.e("Model", "error reading csv data");
+        }
+    }
 
 }
