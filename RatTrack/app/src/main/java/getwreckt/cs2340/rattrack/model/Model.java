df8a829b734a12168bfd1bdfc426f97dfb82a3b5
lastgituser <akikkeri3@gatech.edu>
2017-10-19 17:02:07 -0400

    Removed Model's readFromCSV() method for good

--- RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/model/Model.java
+++ RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/model/Model.java
@@ -36,30 +36,4 @@ public class Model {
 
     public static User getCurrentUser() { return currentUser; }
     public static User setCurrentUser(User user) { return currentUser = user; }
-
-    public static void readCSVFile(InputStream is) {
-        //mDataRef = FirebaseDatabase.getInstance().getReference();
-        //mAuth = FirebaseAuth.getInstance();
-        Log.d("Model", "READING CSV FILE");
-        Log.println(Log.INFO, "Starting", "READING CSV FILE");
-
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
-            String line;
-            br.readLine(); //get rid of header line
-            while(((line = br.readLine()) != null) && line.length() != 0) {
-                String[] sightData = line.split(",");
-                //add new Sighting to list of sightings
-                RatSighting rs = new RatSighting(sightData[0], sightData[1], sightData[7], sightData[8],
-                        sightData[9], sightData[16], sightData[23], sightData[24], sightData[25]);
-                ratSightings.add(rs);
-                //mDataRef.child("ratsightings").child(rs.getUniqueKey()).setValue(rs);
-            }
-            br.close();
-            Log.d("Model", ratSightings.get(0).getUniqueKey());
-            Log.d("Model", ratSightings.get(ratSightings.size() - 1).getUniqueKey());
-        } catch (IOException e) {
-            Log.e("Model", "error reading csv data");
-        }
-    }
 }
