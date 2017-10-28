bd38d63d0369b167a4526bac3ffc7d44e0c97a60
mviust3 <mviust3@gatech.edu>
2017-10-28 13:25:11 -0400

    created sighting manager class

---
+++ RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/model/SightingManager.java
@@ -0,0 +1,16 @@
+package getwreckt.cs2340.rattrack.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Manages rat sightings: filters, sorts, creates, deletes.
+ * all methods should be static
+ * Created by maya v on 10/28/2017.
+ */
+
+public class SightingManager {
+    private static List<RatSighting> ratSightings = new ArrayList<>();
+
+    //
+}
