23214e8325bd042d9a15515a12a702c92ae19b29
lastgituser <akikkeri3@gatech.edu>
2017-11-02 16:57:30 -0400

    Added a DateRangeActivity to select a range of dates.

---
+++ RatTrack/app/src/main/java/getwreckt/cs2340/rattrack/controller/DateRangeActivity.java
@@ -0,0 +1,14 @@
+package getwreckt.cs2340.rattrack.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import getwreckt.cs2340.rattrack.R;
+
+public class DateRangeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_date_range);
+    }
+}
