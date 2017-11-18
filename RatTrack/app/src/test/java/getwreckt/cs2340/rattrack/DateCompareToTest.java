67a747620c57a7735ad9ce6572588ce800be2c97
lastgituser <akikkeri3@gatech.edu>
2017-11-18 18:49:16 -0500

    Added error messages for tests for DateCompateToTest

--- RatTrack/app/src/test/java/getwreckt/cs2340/rattrack/DateCompareToTest.java
+++ RatTrack/app/src/test/java/getwreckt/cs2340/rattrack/DateCompareToTest.java
@@ -15,48 +15,48 @@ public class DateCompareToTest {
     public void testDateTwoMonthsLessThan() {
         Date d1 = new Date("9/2/2017 12:01:00 PM");
         Date d2 = new Date("7/2/2017 12:01:00 PM");
-        Assert.assertTrue(String.format("%d", d2.compareTo(d1)), d2.compareTo(d1) == -2);
+        Assert.assertTrue(String.format("%d, should be -2", d2.compareTo(d1)), d2.compareTo(d1) == -2);
     }
 
     @Test
     public void testDateTwoMonthsGreaterThan() {
         Date d1 = new Date("9/2/2017 12:01:00 PM");
         Date d2 = new Date("7/2/2017 12:01:00 PM");
-        Assert.assertTrue(String.format("%d", d1.compareTo(d2)), d1.compareTo(d2) == 2);
+        Assert.assertTrue(String.format("%d, should be 2", d1.compareTo(d2)), d1.compareTo(d2) == 2);
     }
 
     @Test
     public void testDateTwoDaysGreaterThan() {
         Date d1 = new Date("9/2/2017 12:01:00 PM");
         Date d2 = new Date("9/4/2017 12:01:00 PM");
-        Assert.assertTrue(String.format("%d", d2.compareTo(d1)), d2.compareTo(d1) == 2);
+        Assert.assertTrue(String.format("%d, should be 2", d2.compareTo(d1)), d2.compareTo(d1) == 2);
     }
 
     @Test
     public void testDateTwoYearsLessThan() {
         Date d1 = new Date("9/2/2017 12:01:00 PM");
         Date d2 = new Date("9/2/2015 12:01:00 PM");
-        Assert.assertTrue(String.format("%d", d2.compareTo(d1)), d2.compareTo(d1) == -2);
+        Assert.assertTrue(String.format("%d, should be -2", d2.compareTo(d1)), d2.compareTo(d1) == -2);
     }
 
     @Test
     public void testDateThreeDaysLessThanOverMonth() {
         Date d1 = new Date("9/2/2017 12:01:00 PM");
         Date d2 = new Date("8/31/2017 12:01:00 PM");
-        Assert.assertTrue(String.format("%d", d2.compareTo(d1)), d2.compareTo(d1) == -3);
+        Assert.assertTrue(String.format("%d, should be -3", d2.compareTo(d1)), d2.compareTo(d1) == -3);
     }
 
     @Test
     public void testDateTwoMinutesLessThanOverPM() {
         Date d1 = new Date("9/2/2017 12:01:00 PM");
         Date d2 = new Date("9/2/2017 11:59:00 AM");
-        Assert.assertTrue(String.format("%d", d2.compareTo(d1)), d2.compareTo(d1) == -2);
+        Assert.assertTrue(String.format("%d, should be -2", d2.compareTo(d1)), d2.compareTo(d1) == -2);
     }
 
     @Test
     public void testDateTwoSecondsLessThanOverMinute() {
         Date d1 = new Date("9/2/2017 12:01:01 PM");
         Date d2 = new Date("9/2/2017 12:00:59 PM");
-        Assert.assertTrue(String.format("%d", d2.compareTo(d1)), d2.compareTo(d1) == -2);
+        Assert.assertTrue(String.format("%d, should be -2", d2.compareTo(d1)), d2.compareTo(d1) == -2);
     }
 }
