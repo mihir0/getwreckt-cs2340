4f8872a77b02840de2a47afac5e80b2f70cbf35a
lastgituser <akikkeri3@gatech.edu>
2017-11-17 23:19:08 -0500

    Added a login and register buttons

--- RatTrackiOS/RatTrackiOS/ViewController.swift
+++ RatTrackiOS/RatTrackiOS/ViewController.swift
@@ -9,10 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var screenWidth:CGFloat = UIScreen.main.bounds.size.width
+    var screenHeight:CGFloat = UIScreen.main.bounds.size.height
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let loginButton = UIButton(frame: CGRect(x: 0, y: screenHeight / 5, width: screenWidth, height: screenHeight / 20))
+        loginButton.setTitleColor(UIColor.black, for: .normal)
+        loginButton.backgroundColor = UIColor(red: 46/255, green: 204/255, blue: 113/255, alpha: 1)
+        loginButton.setTitle("Login", for: .normal)
+        self.view.addSubview(loginButton)
+        
+        let registerButton = UIButton(frame: CGRect(x: 0, y: screenHeight / 5 + screenHeight / 20 + screenHeight / 20, width: screenWidth, height: screenHeight / 20))
+        registerButton.setTitleColor(UIColor.black, for: .normal)
+        registerButton.backgroundColor = UIColor(red: 46/255, green: 204/255, blue: 113/255, alpha: 1)
+        registerButton.setTitle("Register", for: .normal)
+        self.view.addSubview(registerButton)
     }
 
     override func didReceiveMemoryWarning() {
