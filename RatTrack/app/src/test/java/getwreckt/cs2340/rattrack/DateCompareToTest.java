8ab344f0dad5ec53829fe860af5a99232f57353c
lastgituser <akikkeri3@gatech.edu>
2017-11-19 18:00:19 -0500

    Made the JUnit tests test for greater than, equal to, or less than 0, and added a couple new tests.

--- RatTrack/app/src/test/java/getwreckt/cs2340/rattrack/DateCompareToTest.java
+++ RatTrack/app/src/test/java/getwreckt/cs2340/rattrack/DateCompareToTest.java
@@ -15,48 +15,76 @@ public class DateCompareToTest {
     public void testDateTwoMonthsLessThan() {
         Date d1 = new Date("9/2/2017 12:01:00 PM");
         Date d2 = new Date("7/2/2017 12:01:00 PM");
-        Assert.assertTrue(String.format("%d, should be -2", d2.compareTo(d1)), d2.compareTo(d1) == -2);
+        Assert.assertTrue(String.format("%d, should be < 0", d2.compareTo(d1)), d2.compareTo(d1) < 0);
     }
 
     @Test
     public void testDateTwoMonthsGreaterThan() {
         Date d1 = new Date("9/2/2017 12:01:00 PM");
         Date d2 = new Date("7/2/2017 12:01:00 PM");
-        Assert.assertTrue(String.format("%d, should be 2", d1.compareTo(d2)), d1.compareTo(d2) == 2);
+        Assert.assertTrue(String.format("%d, should be > 0", d1.compareTo(d2)), d1.compareTo(d2) > 0);
     }
 
     @Test
     public void testDateTwoDaysGreaterThan() {
         Date d1 = new Date("9/2/2017 12:01:00 PM");
         Date d2 = new Date("9/4/2017 12:01:00 PM");
-        Assert.assertTrue(String.format("%d, should be 2", d2.compareTo(d1)), d2.compareTo(d1) == 2);
+        Assert.assertTrue(String.format("%d, should be > 0", d2.compareTo(d1)), d2.compareTo(d1) > 0);
     }
 
     @Test
     public void testDateTwoYearsLessThan() {
         Date d1 = new Date("9/2/2017 12:01:00 PM");
         Date d2 = new Date("9/2/2015 12:01:00 PM");
-        Assert.assertTrue(String.format("%d, should be -2", d2.compareTo(d1)), d2.compareTo(d1) == -2);
+        Assert.assertTrue(String.format("%d, should be < 0", d2.compareTo(d1)), d2.compareTo(d1) < 0);
+    }
+
+    @Test
+    public void testDateTwoYearsLGreaterThan() {
+        Date d1 = new Date("9/2/2015 12:01:00 PM");
+        Date d2 = new Date("9/2/2017 12:01:00 PM");
+        Assert.assertTrue(String.format("%d, should be > 0", d2.compareTo(d1)), d2.compareTo(d1) > 0);
     }
 
     @Test
     public void testDateThreeDaysLessThanOverMonth() {
         Date d1 = new Date("9/2/2017 12:01:00 PM");
         Date d2 = new Date("8/31/2017 12:01:00 PM");
-        Assert.assertTrue(String.format("%d, should be -3", d2.compareTo(d1)), d2.compareTo(d1) == -3);
+        Assert.assertTrue(String.format("%d, should be < 0", d2.compareTo(d1)), d2.compareTo(d1) < 0);
     }
 
     @Test
     public void testDateTwoMinutesLessThanOverPM() {
         Date d1 = new Date("9/2/2017 12:01:00 PM");
         Date d2 = new Date("9/2/2017 11:59:00 AM");
-        Assert.assertTrue(String.format("%d, should be -2", d2.compareTo(d1)), d2.compareTo(d1) == -2);
+        Assert.assertTrue(String.format("%d, should be < 0", d2.compareTo(d1)), d2.compareTo(d1) < 0);
     }
 
     @Test
     public void testDateTwoSecondsLessThanOverMinute() {
         Date d1 = new Date("9/2/2017 12:01:01 PM");
         Date d2 = new Date("9/2/2017 12:00:59 PM");
-        Assert.assertTrue(String.format("%d, should be -2", d2.compareTo(d1)), d2.compareTo(d1) == -2);
+        Assert.assertTrue(String.format("%d, should be < 0", d2.compareTo(d1)), d2.compareTo(d1) < 0);
+    }
+
+    @Test
+    public void testDateFifteenMonthsLessThan() {
+        Date d1 = new Date("9/2/2017 12:01:01 PM");
+        Date d2 = new Date("6/2/2016 12:01:01 PM");
+        Assert.assertTrue(String.format("%d, should be < 0", d2.compareTo(d1)), d2.compareTo(d1) < 0);
+    }
+
+    @Test
+    public void testDateFourHoursLessThanOverDay() {
+        Date d1 = new Date("9/2/2017 12:01:01 AM");
+        Date d2 = new Date("9/1/2017 8:00:59 PM");
+        Assert.assertTrue(String.format("%d, should be < 0", d2.compareTo(d1)), d2.compareTo(d1) < 0);
+    }
+
+    @Test
+    public void testDateSameDate() {
+        Date d1 = new Date("9/2/2017 12:01:01 PM");
+        Date d2 = new Date("9/2/2017 12:01:01 PM");
+        Assert.assertTrue(String.format("%d, should be 0", d2.compareTo(d1)), d2.compareTo(d1) == 0);
     }
 }
