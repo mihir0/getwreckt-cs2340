3e20eba115cb8bc2e95ed4acef546248e555e455
Manan Patel <mpatel360@gatech.edu>
2017-12-02 12:32:46 -0500

    set up app.js and home.js for login/signup

--- RatTrackWeb/js/app.js
+++ RatTrackWeb/js/app.js
@@ -1,1 +1,14 @@
-//
+
+angular.module("myApp", [
+    'ngRoute', 'myApp.home'
+]).
+config(['$routeProvider', function($routeProvider) {
+    //Routes will be here
+
+
+
+    //set default view of our app to home
+    $routeProvider.otherwise({
+        redirectTo: '/home'
+    });
+}])
