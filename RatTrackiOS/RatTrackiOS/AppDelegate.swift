966c4c0aab61ba40ba21559b26dabfe6f12152bf
Mihir Parshionikar <mihir.parshy@hotmail.com>
2017-12-05 01:58:21 -0500

    installed GoogleMaps, implemented demo map w/marker code

--- RatTrackiOS/RatTrackiOS/AppDelegate.swift
+++ RatTrackiOS/RatTrackiOS/AppDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import GoogleMaps
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,6 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        
+        GMSServices.provideAPIKey("AIzaSyB4JRU9kwLkYOA6AjXDgZ59lEZWBhv4dzY")
         // Override point for customization after application launch.
         return true
     }
--- RatTrackiOS/RatTrackiOS/Controller/MapViewController.swift
+++ RatTrackiOS/RatTrackiOS/Controller/MapViewController.swift
@@ -9,9 +9,21 @@
 import Foundation
 import UIKit
 import Firebase
-
+import GoogleMaps
+//API key: AIzaSyB4JRU9kwLkYOA6AjXDgZ59lEZWBhv4dzY
 class MapViewController: UIViewController {
     override func viewDidLoad() {
+        // Create a GMSCameraPosition that tells the map to display the
+        // coordinate -33.86,151.20 at zoom level 6.
+        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        view = mapView
         
+        // Creates a marker in the center of the map.
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+        marker.title = "Sydney"
+        marker.snippet = "Australia"
+        marker.map = mapView
     }
 }
