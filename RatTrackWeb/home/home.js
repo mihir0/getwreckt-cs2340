26054a9dc0b06c4254fb87f90d92075f75ce6caa
Manan Patel <mpatel360@gatech.edu>
2017-12-10 16:00:09 -0500

    changes

--- RatTrackWeb/home/home.js
+++ RatTrackWeb/home/home.js
@@ -11,30 +11,42 @@ angular.module('ratTrack.home', ['ngRoute', 'firebase'])
 }])
 
 // Home controller
-.controller('HomeCtrl', ['$scope', '$firebaseAuth', function($scope, $firebaseAuth) {
-    var firebaseObj = new Firebase("https://cs2340getwreckt.firebaseio.com");
-    var loginObj = $firebaseAuth(firebaseObj);
+.controller('HomeCtrl', ['$scope', '$signInWithEmailAndPassword', function($scope, $signInWithEmailAndPassword) {
+    // var firebaseObj = new Firebase("https://cs2340getwreckt.firebaseio.com");
+    var auth = firebase.auth();
 
     //  On successful authentication, we get a success callback and
     //  on an unsuccessful authentication, we get an error callback.
 
-    $scope.user = {};
+    $scope.user = [];
     $scope.SignIn = function(event) {
         event.preventDefault();  // To prevent form refresh
-        var username = $scope.user.email;
+        var email = $scope.user.email;
         var password = $scope.user.password;
+        if (email && password) {
+            auth.$signInWithEmailAndPassword(email, password).catch(function(error) {
+              // Handle Errors here.
+              console.log('got here');
 
-        loginObj.$authWithPassword('password', {
-                email: username,
-                password: password
-            })
-            .then(function(user) {
-                // Success callback
-                console.log('Authentication successful');
-            }, function(error) {
-                // Failure callback
-                console.log('Authentication failure');
+              var errorCode = error.code;
+              var errorMessage = error.message;
+              console.log(errorMessage);
+
+              // ...
             });
+        }
+
+        // loginObj.$authWithPassword('password', {
+        //         email: username,
+        //         password: password
+        //     })
+        //     .then(function(user) {
+        //         // Success callback
+        //         console.log('Authentication successful');
+        //     }, function(error) {
+        //         // Failure callback
+        //         console.log('Authentication failure');
+        //     });
     }
 
 }]);
